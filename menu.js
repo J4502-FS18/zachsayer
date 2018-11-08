60d9cf514aa38d3de44a0a69a098822ab8957319
zsayer <zacharysayer@mail.missouri.edu>
2018-11-08 13:38:11 -0600

    hamburger menu javascript

---
+++ menu.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.menu-trigger').click(function(){
+      $('.item').slideToggle(500);
+    });
+
+$(window).resize(function(){
+  if ($(window).width()>500){
+    $('.item').removeAttr('style');
+
+  }
+});
+});
